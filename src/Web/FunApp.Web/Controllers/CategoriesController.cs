524ba9f31f7038c66a3906b2cea671c2bc7d43d0
Georgi Dragov <georgi_dragov@abv.bg>
2019-03-01 16:48:33 +0200

    List Categories Joke

---
+++ src/Web/FunApp.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using FunApp.Services.DataServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FunApp.Web.Controllers
+{
+    public class CategoriesController : BaseController
+    {
+        private readonly ICategoriesService categoriesService;
+
+        public CategoriesController(ICategoriesService categoriesService)
+        {
+            this.categoriesService = categoriesService;
+        }
+        public IActionResult Index()
+        {
+            var categories = categoriesService.GetAll().ToList();
+
+            return this.View(categories);
+        }
+
+        public IActionResult Details(int id)
+        {
+            throw new ArgumentException();
+        }
+    }
+}
